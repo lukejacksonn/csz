4124233d663cc7b5eff1dd323ccd5e8c0c9e5cfb
Luke Jackson <lukejacksonn@gmail.com>
2019-04-07 14:58:11 +0100

    Adds stylis for better parsing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import stylis from './stylis.js'
+
 // Keep track of which styles have been added
 // (keyed by hash of a rule set or file path)
 const cache = {}
@@ -7,22 +9,23 @@ const sheet = document.createElement('style')
 document.head.appendChild(sheet)
 
 export default (strings, ...values) => {
-
   // ---------------------------------------
   // A file path was provided
   // ---------------------------------------
 
   if (strings[0].startsWith('/')) {
     // Use the file name as the uid
-    const className = 'csz-' + hash(strings[0])
-    if(!cache[className]) {
-      fetch(strings[0]).then(res => res.text()).then(str => {
-        cache[className] = true
-        // Prefix every rule in the file with the uid and append style
-        sheet.innerHTML += str.replace(/(^[^@\s}]*\s*{)/gm, `\n.${className} $1`)
-      })
+    const className = '.csz-' + hash(strings[0])
+    if (!cache[className]) {
+      fetch(strings[0])
+        .then(res => res.text())
+        .then(str => {
+          cache[className] = true
+          // Prefix every rule in the file with the uid and append style
+          sheet.innerHTML += stylis()(className, str)
+        })
     }
-    return className
+    return className.slice(1)
   }
 
   // ---------------------------------------
@@ -30,73 +33,78 @@ export default (strings, ...values) => {
   // ---------------------------------------
 
   // Zip constant string parts with any interpolated dynamic values
-  const str = strings.reduce((acc, string, i) => acc += string + (values[i] == null ? '' : values[i]), '')
+  const str = strings.reduce(
+    (acc, string, i) => (acc += string + (values[i] == null ? '' : values[i])),
+    ''
+  )
+
   // Use a hash of the ruleset as the uid
-  const className = 'csz-' + hash(str)
-  if(!cache[className]) {
+  const className = '.csz-' + hash(str)
+  if (!cache[className]) {
     cache[className] = true
     // Prefix the rule set with the uid and append style
-    sheet.innerHTML += `\n.${className} { ${str} }`
+    sheet.innerHTML += stylis()(className, str)
   }
-  return className
-
+  return className.slice(1)
 }
 
 // ---------------------------------------
 // Hashing functions
 // ---------------------------------------
 
-function pad (hash, len) {
+function pad(hash, len) {
   while (hash.length < len) {
-    hash = '0' + hash;
+    hash = '0' + hash
   }
-  return hash;
+  return hash
 }
 
-function fold (hash, text) {
-  var i;
-  var chr;
-  var len;
+function fold(hash, text) {
+  var i
+  var chr
+  var len
   if (text.length === 0) {
-    return hash;
+    return hash
   }
   for (i = 0, len = text.length; i < len; i++) {
-    chr = text.charCodeAt(i);
-    hash = ((hash << 5) - hash) + chr;
-    hash |= 0;
+    chr = text.charCodeAt(i)
+    hash = (hash << 5) - hash + chr
+    hash |= 0
   }
-  return hash < 0 ? hash * -2 : hash;
+  return hash < 0 ? hash * -2 : hash
 }
 
-function foldObject (hash, o, seen) {
-  return Object.keys(o).sort().reduce(foldKey, hash);
-  function foldKey (hash, key) {
-    return foldValue(hash, o[key], key, seen);
+function foldObject(hash, o, seen) {
+  return Object.keys(o)
+    .sort()
+    .reduce(foldKey, hash)
+  function foldKey(hash, key) {
+    return foldValue(hash, o[key], key, seen)
   }
 }
 
-function foldValue (input, value, key, seen) {
-  var hash = fold(fold(fold(input, key), toString(value)), typeof value);
+function foldValue(input, value, key, seen) {
+  var hash = fold(fold(fold(input, key), toString(value)), typeof value)
   if (value === null) {
-    return fold(hash, 'null');
+    return fold(hash, 'null')
   }
   if (value === undefined) {
-    return fold(hash, 'undefined');
+    return fold(hash, 'undefined')
   }
   if (typeof value === 'object') {
     if (seen.indexOf(value) !== -1) {
-      return fold(hash, '[Circular]' + key);
+      return fold(hash, '[Circular]' + key)
     }
-    seen.push(value);
-    return foldObject(hash, value, seen);
+    seen.push(value)
+    return foldObject(hash, value, seen)
   }
-  return fold(hash, value.toString());
+  return fold(hash, value.toString())
 }
 
-function toString (o) {
-  return Object.prototype.toString.call(o);
+function toString(o) {
+  return Object.prototype.toString.call(o)
 }
 
-function hash (o) {
-  return pad(foldValue(0, o, '', []).toString(16), 8);
+function hash(o) {
+  return pad(foldValue(0, o, '', []).toString(16), 8)
 }
--- test/test.js
+++ test/test.js
@@ -3,12 +3,22 @@ import css from '../index.js'
 import htm from 'https://unpkg.com/htm?module'
 const html = htm.bind(React.createElement)
 
+const style = {
+  inline: css`
+    background: blue;
+    & button {
+      background: hotpink;
+    }
+  `,
+  file: css`/test/index.css`,
+}
+
 const App = () => {
   const [toggle, setToggle] = React.useState(false)
   return html`
-    <div className=${toggle ? css`/test/index.css` : css`background: blue`}>
+    <div className=${toggle ? style.inline : style.file}>
       <h1>Hello World!</h1>
-      <button className='btn' onClick=${e => setToggle(!toggle)}>Toggle</button>
+      <button className="btn" onClick=${e => setToggle(!toggle)}>Toggle</button>
     </div>
   `
 }
