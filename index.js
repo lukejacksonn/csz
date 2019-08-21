0a5f0459330baac286dc9c00c21afed1fd4c05f7
lukejacksonn <lukejacksonn@gmail.com>
2019-08-21 23:09:50 +0100

    Show after ruleset is appended

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-import stylis from './stylis.js'
+import stylis from './stylis.js';
 
-const cache = {}
+const cache = {};
 const hash = () =>
   Math.random()
     .toString(36)
-    .replace('0.', '')
+    .replace('0.', '');
 
-const sheet = document.createElement('style')
-document.head.appendChild(sheet)
+const sheet = document.createElement('style');
+document.head.appendChild(sheet);
 
-const none = hash => `.${hash}{display:none}`
-const hide = hash => (sheet.innerHTML = none(hash) + sheet.innerHTML)
-const show = hash => (sheet.innerHTML = sheet.innerHTML.replace(none(hash), ''))
+const none = hash => `.${hash}{display:none}`;
+const hide = hash => (sheet.innerHTML = none(hash) + sheet.innerHTML);
+const show = hash =>
+  (sheet.innerHTML = sheet.innerHTML.replace(none(hash), ''));
 
 const process = key => hash => rules => {
-  if (key.startsWith('/')) show(hash)
   sheet.innerHTML += (cache[key] = {
     hash,
-    rules: stylis()(`.${hash}`, rules),
-  }).rules
-}
+    rules: stylis()(`.${hash}`, rules)
+  }).rules;
+  if (key.startsWith('/')) show(hash);
+};
 
 export default (strings, ...values) => {
   const key = strings[0].startsWith('/')
@@ -28,19 +29,19 @@ export default (strings, ...values) => {
         (acc, string, i) =>
           (acc += string + (values[i] == null ? '' : values[i])),
         ''
-      )
+      );
 
-  if (cache[key]) return cache[key].hash
+  if (cache[key]) return cache[key].hash;
 
-  const className = 'csz-' + hash()
-  const append = process(key)(className)
+  const className = 'csz-' + hash();
+  const append = process(key)(className);
 
   if (key.startsWith('/')) {
-    hide(className)
+    hide(className);
     fetch(key)
       .then(res => res.text())
-      .then(append)
-  } else append(key)
+      .then(append);
+  } else append(key);
 
-  return className
-}
+  return className;
+};
